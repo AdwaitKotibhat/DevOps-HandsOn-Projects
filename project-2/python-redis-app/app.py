0b2e69ac81181e12d5ccb4cb3fdd7cc5201169de
adwaitkotibhat <adwaitkotibhatp@gmail.com>
2026-04-13 10:56:11 +0000

    Project-2-Docker-Python-Redis-App

---
+++ project-2/python-redis-app/app.py
@@ -0,0 +1,17 @@
+from flask import Flask
+import redis
+import os
+
+app = Flask(__name__)
+
+# Get Redis host from environment
+redis_host = os.getenv("REDIS_HOST", "localhost")
+r = redis.Redis(host=redis_host, port=6379)
+
+@app.route("/")
+def home():
+    count = r.incr("hits")
+    return f"Hello DevOps 🚀 | Visits: {count}"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)

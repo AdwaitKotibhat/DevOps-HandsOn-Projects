c8c6b810c4b236b44a2acd0b99f571adcd87400d
adwaitkotibhat <adwaitkotibhatp@gmail.com>
2026-04-27 06:22:30 +0000

    Project-8 Deployment Rolling Update Strategu

---
+++ project-8/update-deployment-strategy/app/app.py
@@ -0,0 +1,18 @@
+from flask import Flask
+import os
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    color = os.getenv("APP_COLOR", "unknown")
+    password = os.getenv("DB_PASSWORD", "not-set")
+    return f"""
+App Color: {color}<br>
+DB Password: {password}
+Version: v2<br>
+"""
+    
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
